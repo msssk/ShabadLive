386a09f20183b516e39f02c85f76a6b1200ccd7a
Mangala SSS Khalsa <mkhalsa@sitepen.com>
2017-08-02 17:35:29 -0700

    control: build basic layout GurmukhiKeyboard: add full alphabet

--- control.js
+++ control.js
@@ -1,4 +1,22 @@
 import GurmukhiKeyboard from './widgets/GurmukhiKeyboard.js';
 
+const keyboard = GurmukhiKeyboard.create();
 const footer = document.getElementsByTagName('footer')[0];
-footer.appendChild(GurmukhiKeyboard.create());
+footer.appendChild(keyboard.node);
+
+const searchInput = document.getElementsByClassName('search-input')[0];
+keyboard.node.addEventListener('pointerdown', function (event) {
+	if (event.target.classList.contains('keyboard-letter')) {
+		searchInput.classList.remove('empty');
+		searchInput.children[1].textContent += event.target.textContent;
+	}
+});
+
+const bodyWidth = document.body.offsetWidth;
+const rowPadding = 4 * 2; // 4px padding on left and right
+const keyBorders = 10 * 2; // 1px border on each side of 10 keys
+const desiredKeyWidth = (bodyWidth - rowPadding - keyBorders) / 10;
+const keyRems = 5.5; // 5 from key width, 0.5 from key margins (0.25 * 2)
+const baseFontSize = desiredKeyWidth / keyRems;
+// Round down and drop decimals past the first two to keep it on the safer side of small
+document.documentElement.style.fontSize = (Math.round(baseFontSize * 99) / 100) + 'px';
--- widgets/GurmukhiKeyboard.js
+++ widgets/GurmukhiKeyboard.js
@@ -1,19 +1,53 @@
 export default {
 	create() {
-		const alphabet = [
-			'a', 'A', 'e', 's', 'h'
+		const mainAlphabet = [
+			'a', 'A', 'e', 's', 'h',
+			'k', 'K', 'g', 'G', '|',
+			'c', 'C', 'j', 'J', '\\',
+			't', 'T', 'f', 'F', 'x',
+			'q', 'Q', 'd', 'D', 'n',
+			'p', 'P', 'b', 'B', 'm',
+			'X', 'r', 'l', 'v', 'V',
+			'S', '^', 'Z', 'z', '&'
 		];
 		const node = document.createElement('div');
-		node.className = 'gurmukhi-keyboard';
+		node.className = 'keyboard';
+		let rowNode;
+		let groupNode;
+		let groupIndex = 0;
 
-		alphabet.forEach(function (letter) {
+		mainAlphabet.forEach(function (letter, i) {
+			if (i % 10 === 0) {
+				rowNode = document.createElement('div');
+				rowNode.className = 'keyboard-row';
+				node.appendChild(rowNode);
+			}
+
+			if (i % 5 === 0) {
+				groupNode = document.createElement('div');
+				groupNode.className = 'keyboard-group';
+				rowNode.appendChild(groupNode);
+
+				switch (groupIndex) {
+					case 1:
+					case 2:
+					case 5:
+					case 6:
+						groupNode.classList.add('alt');
+				}
+
+				groupIndex++;
+			}
+
 			const letterNode = document.createElement('div');
 
-			letterNode.className = 'letter';
+			letterNode.className = 'keyboard-letter';
 			letterNode.textContent = letter;
-			node.appendChild(letterNode);
+			groupNode.appendChild(letterNode);
 		});
 
-		return node;
+		return {
+			node
+		};
 	}
 };
