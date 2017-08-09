debdabc8dec3729548542be68df9896deed5eb25
Mangala SSS Khalsa <mkhalsa@sitepen.com>
2017-08-08 21:12:46 -0700

    add search demo

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
 (function () {
-	
+	const containerNode = document.getElementsByClassName('container')[0];
+	containerNode.addEventListener('click', function (event) {
+		if (event.target.classList.contains('close')) {
+			event.target.parentNode.classList.add('hidden');
+		}
+	});
+
+	const searchDemoNode = document.getElementById('searchDemo');
+
+	function showSearchDemo () {
+		searchDemoNode.classList.remove('hidden');
+	}
+
+	const demoButton1 = document.getElementById('demo1');
+	demoButton1.addEventListener('click', showSearchDemo);
 })();
