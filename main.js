70c690e4347e8e0ea397b3e11af2c3c78992c422
Mangala SSS Khalsa <mkhalsa@sitepen.com>
2017-08-09 23:11:34 -0700

    add shabad demos

--- main.js
+++ main.js
@@ -1,19 +1,85 @@
 (function () {
-	const containerNode = document.getElementsByClassName('container')[0];
-	containerNode.addEventListener('click', function (event) {
-		if (event.target.classList.contains('close')) {
-			event.target.parentNode.classList.add('hidden');
+	let activeDemo;
+	let activeShabad;
+	let shabadNodes = document.body.getElementsByClassName('shabad');
+
+	function showDemo (id) {
+		const demoNode = document.getElementById(id);
+
+		if (demoNode) {
+			demoNode.classList.remove('hidden');
+			activeDemo = demoNode;
+
+			if (id === 'shabadDemo') {
+				const shabadNode = shabadNodes[0];
+				activeShabad = shabadNode;
+				resizeShabad(shabadNode);
+			}
+		}
+	}
+
+	function hideDemo () {
+		if (activeDemo) {
+			activeDemo.classList.add('hidden');
+			activeDemo = null;
+		}
+	}
+
+	function resizeShabad (shabadNode) {
+		shabadNode = shabadNode || activeShabad;
+		const parentHeight = shabadNode.parentElement.parentElement.offsetHeight;
+		const parentWidth = shabadNode.parentElement.parentElement.offsetWidth;
+		const nodeHeight = shabadNode.offsetHeight;
+		const nodeWidth = shabadNode.offsetWidth;
+		const heightRatio = (parentHeight / nodeHeight) * 0.9;
+		const widthRatio = (parentWidth / nodeWidth) * 0.9;
+		const scaleValue = Math.min(heightRatio, widthRatio);
+		shabadNode.style.transform = 'scale(' + scaleValue + ')';
+		shabadNode.style.left = ((parentWidth - nodeWidth) / 2) + 'px';
+		shabadNode.style.top = ((parentHeight - nodeHeight) / 2) + 'px';
+	}
+
+	function rotateShabad (direction) {
+		if (direction === 'prev') {
+			nextNode = activeShabad.parentElement.previousElementSibling;
+			if (!nextNode) {
+				nextNode = shabadNodes[shabadNodes.length - 1].parentElement;
+			}
+		}
+		else {
+			nextNode = activeShabad.parentElement.nextElementSibling;
+			if (!nextNode) {
+				nextNode = shabadNodes[0].parentElement;
+			}
+		}
+		activeShabad.parentElement.classList.add('hidden');
+		nextNode.classList.remove('hidden');
+		activeShabad = nextNode.getElementsByClassName('shabad')[0];
+		resizeShabad(activeShabad);
+	}
+
+	document.body.addEventListener('click', function (event) {
+		if (event.target.classList.contains('demoButton')) {
+			showDemo(event.target.dataset.demoId);
+		}
+		else if (event.target.classList.contains('close')) {
+			hideDemo();
+		}
+		else if (event.target.classList.contains('navicon')) {
+			if (event.target.classList.contains('prev')) {
+				rotateShabad('prev');
+			}
+			else {
+				rotateShabad('next');
+			}
 		}
 	});
 
-	const searchDemoNode = document.getElementById('searchDemo');
-
-	function showSearchDemo () {
-		searchDemoNode.classList.remove('hidden');
-	}
-
-	const demoButton1 = document.getElementById('demo1');
-	demoButton1.addEventListener('click', showSearchDemo);
+	document.body.addEventListener('keydown', function (event) {
+		if (event.keyCode === /* escape */ 27) {
+			hideDemo();
+		}
+	});
 })();
 
 (function () {
